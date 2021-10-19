979ae5891edcdedfc5310d5c46dcbfda24048e47
Daniela Vargas <daniela.03v@gmail.com>
2021-10-18 19:31:07 -0500

    se agrega mapper

---
+++ src/main/java/com/example/mapper/RecursoMapper.java
@@ -0,0 +1,37 @@
+package com.example.mapper;
+
+import com.example.bibliotecawebflux.dto.RecursoDTO;
+import com.example.bibliotecawebflux.model.Recurso;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class RecursoMapper {
+
+    public Function<RecursoDTO, Recurso> dtoToRecurso(String id) {
+        return updateDato -> {
+            var recurso = new Recurso();
+            recurso.setId(id);
+            recurso.setNombreRecurso(updateDato.getNombreRecurso());
+            recurso.setDisponible(updateDato.getDisponible());
+            recurso.setTipoRecursoId(updateDato.getTipoRecursoId());
+            recurso.setCategoriaId(updateDato.getCategoriaId());
+            recurso.setFecha(updateDato.getFecha());
+            return recurso;
+        };
+    }
+
+    public Function<Recurso, RecursoDTO> RecursoToDTO() {
+        return recurso -> {
+            var recursoDTO = new RecursoDTO();
+            recursoDTO.setId(recurso.getId());
+            recursoDTO.setFecha(recurso.getFecha());
+            recursoDTO.setDisponible(recurso.getDisponible());
+            recursoDTO.setNombreRecurso(recurso.getNombreRecurso());
+            recursoDTO.setTipoRecursoId(recurso.getTipoRecursoId());
+            recursoDTO.setCategoriaId(recurso.getCategoriaId());
+            return recursoDTO;
+        };
+    }
+}
---
+++ src/main/java/com/example/mapper/RespuestaRecursoMapper.java
@@ -0,0 +1,40 @@
+package com.example.mapper;
+
+import com.example.bibliotecawebflux.dto.RecursoDTO;
+import com.example.bibliotecawebflux.dto.RespuestaRecursoDTO;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class RespuestaRecursoMapper {
+    private Date objDate = new Date();
+    private String strDateFormat = "hh: mm: ss a dd-MMM-aaaa";
+    private SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+
+    public Function<RecursoDTO, RespuestaRecursoDTO> consultarRecurso() {
+     return recurso -> {
+         RespuestaRecursoDTO respuestaRecursoDTO = new RespuestaRecursoDTO();
+         String descripcion = recurso.getDisponible() ? "El recurso esta disponible" : "El Libro no esta disponible";
+         respuestaRecursoDTO.setDescripcion(descripcion);
+         respuestaRecursoDTO.setDisponible(recurso.getDisponible());
+         respuestaRecursoDTO.setFecha(recurso.getFecha());
+         return respuestaRecursoDTO;
+     };
+    }
+
+    public Function<RecursoDTO,RespuestaRecursoDTO> prestarRecurso() {
+        return recurso -> {
+          RespuestaRecursoDTO respuestaRecursoDTO = new RespuestaRecursoDTO();
+          String descripcion = "";
+          respuestaRecursoDTO.setDescripcion(descripcion);
+          respuestaRecursoDTO.setDisponible(recurso.getDisponible());
+          respuestaRecursoDTO.setFecha(recurso.getFecha());
+          return respuestaRecursoDTO;
+        };
+
+    }
+}
