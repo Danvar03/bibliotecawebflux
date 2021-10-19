c4e80d82e6f768a45319c18a6b46f3d8a33bbc04
Daniela Vargas <daniela.03v@gmail.com>
2021-10-18 20:49:27 -0500

    Se agregan test CrearRecurso, ConsultarDisponibilidadRecurso, EliminarRecurso

---
+++ src/test/java/com/example/bibliotecawebflux/usecase/recurso/CasoUsoCrearTest.java
@@ -0,0 +1,46 @@
+package com.example.bibliotecawebflux.usecase.recurso;
+
+import com.example.bibliotecawebflux.dto.RecursoDTO;
+import com.example.bibliotecawebflux.model.Recurso;
+import com.example.bibliotecawebflux.repositories.RepositorioRecurso;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CasoUsoCrearTest {
+
+    @SpyBean
+    private CasoUsoCrear casoUsoCrear;
+
+    @MockBean
+    private RepositorioRecurso repositorioRecurso;
+
+    @Test
+    public void crearRecurso() {
+
+        var recursoDTO = new    RecursoDTO("1","18/10/2021", true, "GOT", "1A","1L");
+        var recurso = new Recurso();
+
+        recurso.setId("1");
+        recurso.setFecha("18/10/2021");
+        recurso.setDisponible(true);
+        recurso.setNombreRecurso("GOT");
+        recurso.setCategoriaId("1A");
+        recurso.setTipoRecursoId("1L");
+
+        when(repositorioRecurso.save(Mockito.any(Recurso.class))).thenReturn(Mono.just(recurso));
+
+        var result = casoUsoCrear.apply(recursoDTO);
+        Assertions.assertEquals(result.block(), "1");
+    }
+
+
+}
